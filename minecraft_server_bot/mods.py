4f2afb5653447fe94a303fd3597654d57dcdea5f
William Lee <wlee753159@gmail.com>
2024-10-20 04:14:25 +0100

    Decode bytes before loading Forge mods.toml

--- minecraft_server_bot/mods.py
+++ minecraft_server_bot/mods.py
@@ -32,5 +32,5 @@ class Mod:
                 data = json.load(file.open("fabric.mod.json"))
                 return cls.from_fabric_data(data)
             elif "META-INF/mods.toml" in members:
-                data = toml.loads(file.open("META-INF/mods.toml").read())
+                data = toml.loads(file.open("META-INF/mods.toml").read().decode())
                 return cls.from_forge_data(data)
